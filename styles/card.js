6dfb1b5abef2ccc7d330a92cbacbc51d543e5c15
Chelsea S <118695281+cswebdev@users.noreply.github.com>
2023-01-16 22:40:26 -0500

    Renaming system directory

--- styles/card.js
+++
@@ -1,8 +0,0 @@
-// define a constructor for the card
-function Card(suit, value) {
-   this.suit = suit;
-   this.value = value;
-}
-
-// export the card constructor as the default export
-export default Card;
